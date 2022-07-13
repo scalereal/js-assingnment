49b876fda7051918a4fd26612979e649875aeba0
unknown <fithaashraf172@gmail.com>
2022-07-13 15:56:00 +0530

    change is applied

--- main.js
+++ main.js
@@ -1,65 +1,56 @@
-const key="18acb5642976765d66ed9ea326327f5c";
-const  BASEURL = "https://api.themoviedb.org/3";
-const APIURL =
-    `${BASEURL}/discover/movie?sort_by=popularity.desc&api_key=${key}`;
+const key = "18acb5642976765d66ed9ea326327f5c";
+const BASEURL = "https://api.themoviedb.org/3";
+const APIURL = `${BASEURL}/discover/movie?sort_by=popularity.desc&api_key=${key}`;
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =`${BASEURL}/search/movie?&api_key=${key}`;
-
-const section=document.querySelector('.section');
+const SEARCHAPI = `${BASEURL}/search/movie?&api_key=${key}`;
+const section = document.querySelector(".section");
 const main = document.querySelector(".main");
 const movielist = document.querySelector(".movielist");
-const form= document.querySelector(".form");
-const search= document.querySelector(".search");
-
-const getMovies=async (api)=>{
-  const response=await fetch(api);
-  const data=await response.json();
-  // console.log(data);
-  if(data.results.length>0){
-  showMovies(data.results.slice(0,15));
-  section.style.display="none";
-  movielist.style.display="block";
-     } 
-   else{
-    console.log(data);
-
-        
-
-         section.style.display="block";
-         movielist.style.display="none";
- 
-     }
-    }
-getMovies(APIURL);
-
-const showMovies=(data)=>{
-  console.log(data)
-  main.innerHTML=" ";
-  data.forEach(
-    (result)=>{const imagePath = result.poster_path === null ? "no-image-icon-13.png" : IMGPATH + result.poster_path;
-    const {poster_path,title,}= result;
-    const box=document.createElement('div');
-    box.classList.add("card");
-    box.innerHTML=`<img src="${imagePath}" alt="" />
-    <h3 class="card-item"> ${title} </h3>
-    <button class ="btn"> Book Now</button>
-    `
-    main.appendChild(box);
+const form = document.querySelector(".form");
+const search = document.querySelector(".search");
+let query = "";
+const getMovies = async (query) => {
+  const url = query === "" ? APIURL : SEARCHAPI + "&query=" + query;
+  const response = await fetch(url);
+  const data = await response.json();
+  if (data.results.length > 0) {
+    showMovies(data.results.slice(0, 15));
+    section.style.display = "none";
+    movielist.style.display = "block";
+  } else {
+    section.style.display = "block";
+    movielist.style.display = "none";
   }
-  )
-}
+};
+getMovies(query);
 
+const showMovies = (data) => {
+  data.forEach((result) => {
+    const imagePath =
+      result.poster_path === null
+        ? "no-image-icon-13.png"
+        : IMGPATH + result.poster_path;
+    const { poster_path, title } = result;
+    const box = document.createElement("div");
 
+    box.classList.add("card");
+    const h3 = document.createElement("h3");
+    h3.classList.add("card-item");
+    h3.innerText = title;
+    const img = document.createElement("img");
+    img.src = imagePath;
+    const button = document.createElement("button");
+    button.classList.add("btn");
+    button.innerText = "Book Now";
+    box.appendChild(img);
+    box.appendChild(h3);
+    box.appendChild(button);
+    main.appendChild(box);
+  });
+};
 
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const searchTerm= search.value ;
-  if(searchTerm){
-    getMovies((SEARCHAPI)+ '&query=' +(searchTerm));
-
-} else{
-  getMovies(APIURL)
-}
-})
- 
-
+  const searchTerm = search.value;
+  getMovies(searchTerm);
+});
