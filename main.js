d30d832045e41645f292b972a7cb2d81f0d94486
ubed-ali <syedubed09@gmail.com>
2022-08-03 10:36:25 +0530

    US | js-assignment

--- main.js
+++ main.js
@@ -1,6 +1,69 @@
-import './style.css'
+const API_KEY = 'api_key=ba527e40ac8ca2c3848e65d14b95a2d7';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const movieNotFound = document.getElementById(".movie-not-found")
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+
+getMovies(API_URL);
+
+function getMovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results.slice(0,15))
+        showMovies(data.results.slice(0,15));
+    })
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src="${IMG_URL+poster_path}" alt="${title}">
+                <div class="movie-info">
+                  <h3>${title}</h3>
+                  <span class="${getColor(vote_average)}">${vote_average}</span>
+                </div>
+      
+                <div class="overview">
+                  <h3>Overview</h3>
+                  ${overview}
+                </div>
+        
+        `
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getColor(vote) {
+    if(vote>=8) {
+        return "green"
+    } else if(vote >= 5) {
+        return "orange"
+    } else {
+        return "red"
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    } else {
+        main.style.display = "none";
+        movieNotFound.style.display = "block";
+    }
+})
