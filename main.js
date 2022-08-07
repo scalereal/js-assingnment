05b52adcb6aabbd960da919636e8d4b2a0c72193
pankti77 <pankti77dave@gmail.com>
2022-08-07 19:13:09 +0530

    PD | added js for no-results-page

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-const movieGrid = document.querySelector("#movie-grid")
+const movieGrid = document.querySelector("#movie_grid")
 const getMovies = async (api) => {
     const response = await fetch(api)
     const data = await response.json()
     let display_size = data.results.slice(0,15)
-    console.log(display_size);
-    showMovies(display_size)
+    var movie_grid = document.getElementById("movie_grid");
+    console.log(movie_grid)
+    var no_results = document.getElementById("no_results_wrapper");
+    if (display_size.length) {
+        movie_grid.style.display = "flex";
+        no_results.style.display = "none";
+        showMovies(display_size);
+      } else {
+        movie_grid.style.display = "none";
+        no_results.style.display = "block";
+      }
 }
 const showMovies = (data) => {
     movieGrid.innerHTML = "";
