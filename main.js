25ff263ae9e696d9c157c60bc77911826db7275f
pankti77 <pankti77dave@gmail.com>
2022-08-08 10:34:45 +0530

    removed var and used let instead of it

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const getMovies = async (api) => {
     const response = await fetch(api)
     const data = await response.json()
     let display_size = data.results.slice(0,15)
-    var movie_grid = document.getElementById("movie_grid");
+    let movie_grid = document.getElementById("movie_grid");
     console.log(movie_grid)
-    var no_results = document.getElementById("no_results_wrapper");
+    let no_results = document.getElementById("no_results_wrapper");
     if (display_size.length) {
         movie_grid.style.display = "flex";
         no_results.style.display = "none";
@@ -24,16 +24,16 @@ const showMovies = (data) => {
         (item) => {
             const movieGridItem = document.createElement("div")
             movieGridItem.classList.add("movie-grid-item")
-            var movieGridInnerTag = document.createElement("div")
+            let movieGridInnerTag = document.createElement("div")
             movieGridInnerTag.classList.add("movie-grid-inner")
-            var imgTag = document.createElement("img")
+            let imgTag = document.createElement("img")
             imgTag.src = img_path + item.poster_path 
             imgTag.alt = "banner-image"
-            var movieContentTag = document.createElement("div")
+            let movieContentTag = document.createElement("div")
             movieContentTag.classList.add("movie-content")
-            var h3Tag = document.createElement("h3")
+            let h3Tag = document.createElement("h3")
             h3Tag.classList.add("h3-tag")
-            var anchorTag = document.createElement("a")
+            let anchorTag = document.createElement("a")
             anchorTag.classList.add("book-now-btn")
             anchorTag.title = "book-now-btn"
             anchorTag.innerHTML = "book now"
@@ -48,12 +48,12 @@ const showMovies = (data) => {
     )
 }
 getMovies(api_url);
-var search_box = document.getElementById("search_bar_wrapper");
+let search_box = document.getElementById("search_bar_wrapper");
 search_box.addEventListener(
     "submit",
     function(e){
         e.preventDefault();
-        var search = document.getElementById("search")
+        let search = document.getElementById("search")
         if (search.value != "") {
             getMovies(search_api + search.value)
         } else {
