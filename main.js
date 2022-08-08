a69b23241c194760bbd5a36af1cccc77a511c322
ubed-ali <syedubed09@gmail.com>
2022-08-08 18:48:21 +0530

    US | js-assignment

--- main.js
+++ main.js
@@ -3,18 +3,26 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-const movieNotFound = document.getElementById(".movie-not-found")
-
+const movieNotFound = document.getElementById('movie-not-found');
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+let navbar = document.getElementById("navbar");
 
+const toggle =() =>{
+	if(navbar.style.display == "none") {
+		navbar.style.display = "block"
+		navbar.style.display = "left"
+	}
+	else {
+		navbar.style.display = "none"
+	}
+}
 
 getMovies(API_URL);
 
 function getMovies(url) {
     fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results.slice(0,15))
         showMovies(data.results.slice(0,15));
     })
 }
@@ -22,27 +30,30 @@ function getMovies(url) {
 function showMovies(data) {
     main.innerHTML = '';
 
-
-    data.forEach(movie => {
+		if (data.length) {
+			data.forEach(movie => {
         const {title, poster_path, vote_average, overview} = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
         <img src="${IMG_URL+poster_path}" alt="${title}">
-                <div class="movie-info">
-                  <h3>${title}</h3>
-                  <span class="${getColor(vote_average)}">${vote_average}</span>
-                </div>
-      
-                <div class="overview">
-                  <h3>Overview</h3>
-                  ${overview}
-                </div>
-        
-        `
+				<div class="movie-info">
+					<h3>${title}</h3>
+					<span class="${getColor(vote_average)}">${vote_average}</span>
+				</div>
 
+				<div class="overview">
+					<h3>Overview</h3>
+					${overview}
+				</div>
+        `
         main.appendChild(movieEl);
+		movieNotFound.style.display = "none";
     });
+		} else {
+			movieNotFound.style.display = "block";
+		}
+    
 }
 
 function getColor(vote) {
@@ -55,15 +66,13 @@ function getColor(vote) {
     }
 }
 
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchTerm = search.value;
-
-    if(searchTerm) {
-        getMovies(searchURL+'&query='+searchTerm)
-    } else {
-        main.style.display = "none";
-        movieNotFound.style.display = "block";
+		getMovies(searchURL+'&query='+searchTerm);
+    if(main.style.display == "none") {
+			main.style.display = movieNotFound
+		} else {
+			getMovies(searchURL+'&query='+searchTerm)
     }
 })
