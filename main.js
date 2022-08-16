a60ac2b2c30dc5776d75295c9ddef3aff124cf22
pankti77 <pankti77dave@gmail.com>
2022-08-16 12:35:55 +0530

    modified image width in js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const api_url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const img_path = "https://image.tmdb.org/t/p/w1280";
+const img_path = "https://image.tmdb.org/t/p/w500";
 const search_api = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 const movieGrid = document.querySelector("#movie_grid")
 const getMovies = async (api) => {
