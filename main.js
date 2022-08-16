6b5f4a411830c7a907e120aa0ca8d8f1d0855332
grv980 <gp.aarush@gmail.com>
2022-08-17 00:38:54 +0530

    GP | COMPLETED

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 const API_URL= "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3085793d7e6567a4efd497a4b8f80a2b"
 
 const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
@@ -6,55 +7,70 @@ const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=3085793d7e
 const form  = document.getElementById('form')
 const search=document.getElementById('search')
 const main=document.getElementById('main')
+const page_Notfound = document.querySelector('.app')
+const count =0;
 
+ const displayMovie=(collection)=> {
+  main.innerHTML = "";
+  collection.forEach((item,index) => {
+    if(index<15){
 
-getMovies(API_URL)
-async function getMovies(url){
-  const res= await fetch(url)
-  const data= await res.json()
-  displayMovies(data.results)
-  console.log(data.results);
+    
+    const { title, poster_path } = item;
+    const imageDiv = document.createElement("article");
+    imageDiv.classList.add("card");
+    imageDiv.style.backgroundImage = `url(${IMAGE_PATH + poster_path})`;
+    imageDiv.innerHTML = `
+    <h3  class="title" >${title}</h3>
+    <button class="button" > Book Now </button>
+    `;
+    main.appendChild(imageDiv);
+  }});
 }
-function displayMovies(movies){
-  main.innerHTML=''
-  movies.forEach((movie) => {
-    const{title,poster_path,vote_average,overview} =movie
-    const moviesElement=document.createElement('div')
-    console.log(IMAGE_PATH+ poster_path)
-    moviesElement.classList.add('movie')
-    moviesElement.innerHTML=`
-    <img src="${IMAGE_PATH + poster_path}" alt="${title}"/>
-    <div class='movie-info'>
-    <h3>${title}</h3>
-    <span class ="${getClassesByRating(vote_average)}">${vote_average}</span>
-   <div class='overview'>
-   <h3>Overview</h3>
-   ${overview}
-   </div>
-   
-    </div>
-    `
+ const fetchMovies=async(url)=> {
+  const res = await fetch(url);
+  const data = await res.json();
+  const result = data.results;
 
-    main.appendChild(moviesElement)
-  });
-}
-function getClassesByRating(rating){
-  if(rating>=8){
-    return 'green'
-  }else if(rating>=5){
-    return 'orange'
-  }else{
-    return 'red'
+  
+  
+  if (result) {
+    displayMovie(result);
+    page_Notfound.style.display = "none";
   }
-}
-form.addEventListener('submit',(e)=>{
-   e.preventDefault()
-  const searchValue = search.value
-  console.log(searchValue)
-  if(searchValue && searchValue !==''){
-    getMovies(SEARCH_URL+searchValue)
-    searchValue=''
-  }else{
-    window.location.reload()
+  if (result.length === 0) {
+    
+    page_Notfound.style.display = "block"
+    const newDiv = document.createElement("article");
+    newDiv.classList.add("container");
+    newDiv.innerHTML = `
+    <h2>Sorry,there is no result for keyword you searched</h2>
+    <img src="background.png" alt="background">
+    `;
+    if(count === 0){
+      page_Notfound.appendChild(newDiv);  
+      count = 1;
+    }
   }
-})
+  
+}
+
+fetchMovies(API_URL);
+
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = search.value;
+
+  if (value) {
+    fetchMovies(SEARCH_URL + value); 
+    
+  } else {
+    fetchMovies(API_URL);
+    search.value = '' ;
+  }
+});
+
+
+
