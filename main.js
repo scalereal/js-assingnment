d2872555638822d5d6f975d8f9ab8cd8bfa56956
grv980 <gp.aarush@gmail.com>
2022-08-16 18:40:54 +0530

    GP | Search function is working

--- main.js
+++ main.js
@@ -1,79 +1,60 @@
-const { formatPostcssSourceMap } = require("vite");
 
-const API_KEY = "api_key=3085793d7e6567a4efd497a4b8f80a2b";
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const API_URL= "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3085793d7e6567a4efd497a4b8f80a2b"
 
-
-const main = document.getElementById('main');
+const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
+const SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=3085793d7e6567a4efd497a4b8f80a2b&query=";
+const form  = document.getElementById('form')
+const search=document.getElementById('search')
+const main=document.getElementById('main')
 
 
-getMovies(API_URL);
-
-
-function getMovies(url) {
-
-fetch(url).then(res=> res.json()).then(data=>{
-
-  showMovies(data.results);
-})
-
-
+getMovies(API_URL)
+async function getMovies(url){
+  const res= await fetch(url)
+  const data= await res.json()
+  displayMovies(data.results)
+  console.log(data.results);
 }
+function displayMovies(movies){
+  main.innerHTML=''
+  movies.forEach((movie) => {
+    const{title,poster_path,vote_average,overview} =movie
+    const moviesElement=document.createElement('div')
+    console.log(IMAGE_PATH+ poster_path)
+    moviesElement.classList.add('movie')
+    moviesElement.innerHTML=`
+    <img src="${IMAGE_PATH + poster_path}" alt="${title}"/>
+    <div class='movie-info'>
+    <h3>${title}</h3>
+    <span class ="${getClassesByRating(vote_average)}">${vote_average}</span>
+   <div class='overview'>
+   <h3>Overview</h3>
+   ${overview}
+   </div>
+   
+    </div>
+    `
 
-
-function showMovies(data){
-  main.innerHTML = '';
-  
-  
-    data.forEach(movie => {
-      const{title, poster_path,vote_average,overview} = movie;
-      const movieE1 = document.createElement('div');
-      movieE1.classList.add('movie');
-      movieE1.innerHTML = `
-  
-      <img src="${IMG_URL+poster_path}" alt="${title}">
-  
-          <div class="movie-info">
-            <h3>${title}</h3>
-            <span class="${getColor(vote_average)}">${vote_average}</span>
-          </div>
-  
-          <div class="Overview">
-  
-            <h3>Overview</h3>
-            ${overview}
-          </div>
-        
-        `
-      main.appendChild(movieE1);
-  
-    })
+    main.appendChild(moviesElement)
+  });
 }
-  
-function getColor(vote){
-    if(vote>=8){
-      return'green'
-  
-    }else if(vote>=5){
-      return 'orange'
-    }else {
-      return 'red'
-    }
-}
-
-form.addEventLIstener('submit',(e) => {
-  e.preventDefault();
-
-  const searchTerm = search.value;
-
-  if(searchTerm){
-    getMovies(searchURL+'&query='+searchTerm)
+function getClassesByRating(rating){
+  if(rating>=8){
+    return 'green'
+  }else if(rating>=5){
+    return 'orange'
   }else{
-    getMovies(API_URL);
+    return 'red'
+  }
+}
+form.addEventListener('submit',(e)=>{
+   e.preventDefault()
+  const searchValue = search.value
+  console.log(searchValue)
+  if(searchValue && searchValue !==''){
+    getMovies(SEARCH_URL+searchValue)
+    searchValue=''
+  }else{
+    window.location.reload()
   }
 })
-  
-  
