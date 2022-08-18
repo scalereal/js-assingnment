bafcbceb49f911c705c0e6184f287159d7594be2
ubed-ali <syedubed09@gmail.com>
2022-08-18 11:40:57 +0530

    US | js-assignment | Resolved searchbar issue

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-let navbar = document.getElementById("navbar");
+let navbar = document.getElementById('navbar');
 const main = document.getElementById('main');
 const movieNotFound = document.getElementById('movie-not-found');
+let node = 0
 
 const toggle =() =>{
 	if(navbar.style.display == "none") {
-		navbar.style.display = "block"
+		navbar.style.display = "inline-block"
 		navbar.style.display = "left"
 	}
 	else {
@@ -19,52 +20,55 @@ const toggle =() =>{
 	}
 }
 
+
 getMovies(API_URL);
+
 
 function getMovies(url) {
     fetch(url).then(res => res.json()).then(data => {
-        showMovies(data.results.slice(0,15));
-    })
+				showMovies(data.results.slice(0,15));			
+		})
 }
 
+ 
 function showMovies(data) {
-		if (data.length) {
-			data.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie;
+	main.innerHTML = '';
+	if (data.length) {
+		data.forEach(movie => {
+			const {title, poster_path, vote_average, overview} = movie;
 		
-		const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
+			const movieEl = document.createElement('div');
+			movieEl.classList.add('movie');
 
-		const movieInfo = document.createElement('div');
-        movieInfo.classList.add('movie-info');
+			const movieInfo = document.createElement('div');
+			movieInfo.classList.add('movie-info');
 
-		const movieOverview = document.createElement('div');
-        movieOverview.classList.add('overview');
+			const movieOverview = document.createElement('div');
+			movieOverview.classList.add('overview');
 
-		const movie_poster = document.createElement('img')
-		movie_poster.src = IMG_URL+poster_path
+			const movie_poster = document.createElement('img')
+			movie_poster.src = IMG_URL+poster_path
 
-		const movie_name = document.createElement('h3')
-		movie_name.innerText = title
+			const movie_name = document.createElement('h3')
+			movie_name.innerText = title
 
-		const rating = document.createElement('span')
-		rating.innerHTML = vote_average
-		rating.classList.add(getColor(vote_average))
+			const rating = document.createElement('span')
+			rating.innerHTML = vote_average
+			rating.classList.add(getColor(vote_average))
 
-		const movie_overview = document.createElement('h3')
-		movie_overview.innerText = overview
+			const movie_overview = document.createElement('h3')
+			movie_overview.innerText = overview
 
-
-		movieInfo.appendChild(movie_name)
-		movieInfo.appendChild(rating)
-		movieOverview.appendChild(movie_overview)
-
-		movieEl.append(movie_poster, movieInfo, movieOverview)
-		main.appendChild(movieEl)
-        
-		movieNotFound.style.display = "none";
-    });
+			movieInfo.append(movie_name, rating)
+			movieOverview.appendChild(movie_overview)
+			movieEl.append(movie_poster, movieInfo, movieOverview)
+			main.appendChild(movieEl)
+			
+			main.style.display = "flex";
+			movieNotFound.style.display = "none";
+		});
 	} else {
+		main.style.display = "none";
 		movieNotFound.style.display = "block";
 	}
     
@@ -81,12 +85,16 @@ function getColor(vote) {
 }
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const searchTerm = search.value;
+	e.preventDefault();
+	const searchTerm = search.value;
+	if(searchTerm){
 		getMovies(searchURL+'&query='+searchTerm);
-    if(main.style.display == "none") {
+	} else{
+		getMovies(API_URL)
+		if(main.style.display == "none") {
 			main.style.display = movieNotFound
 		} else {
 			getMovies(searchURL+'&query='+searchTerm)
-    }
+		}
+	}
 })
